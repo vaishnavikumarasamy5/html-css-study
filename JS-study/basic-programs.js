249f0387d0871555e80cc812e7a91efb9d037538
Vaishnavi <vaishnavikumarasamy5@gmail.com>
2021-10-17 19:51:59 +0000

    add new syntax

--- JS-study/basic-programs.js
+++ JS-study/basic-programs.js
@@ -1,2 +1,5 @@
 //Print date in the console
 console.log('The current date is',new Date());
+// Print Date in the console
+const now = new Date();
+console.log('The current date is ', new Date(), 'and its data type is ', typeof now);
