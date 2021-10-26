d5f7b984a14509cc53198738199ad91b4b96eacf
Vaishnavi <vaishnavikumarasamy5@gmail.com>
2021-10-26 18:39:13 +0000

    changes

--- JS-study/arithmetic.js
+++ JS-study/arithmetic.js
@@ -1,24 +1,28 @@
 //sum of two numbers
 function calcSum(a, b) {
-    console.log("sum of two number is,",a+b);
+    console.log("sum of two number is,", a + b);
 }
 calcSum(5, 6);
 
 //multiple of two numbers
-function calcMulitple(a,b){
-    console.log("mulitple of two numbers is,",a*b);
-}
-calcMulitple(4,3);
-
+function calcMultipe(a, b) {
+    if(a,b===typeof Number){
+        console.log("multiplication of two number is", a * b)
+}else{
+      console.log("check whether the give value is in number")
+    }
+}  
+    
+calcMultipe("hello", 4);
 
 //subraction of two numbers
-function calcSubraction(a,b){
-    console.log("subraction of two numbers is,",a-b);
+function calcSubraction(a, b) {
+    console.log("subraction of two numbers is,", a - b)
 }
-calcSubraction(8,4);
+calcSubraction(8, 4);
 
 //division of two numbers
-function calcDivision(a,b){
-    console.log("division of two numbers is,",a/b);
+function calcDivision(a, b) {
+    console.log("division of two numbers is,", a / b);
 }
-calcDivision(33,3);
+calcDivision(33, 3);
