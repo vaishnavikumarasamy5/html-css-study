e650e1a5404fd4020ed2420a1800165856a8b13a
Vaishnavi <vaishnavikumarasamy5@gmail.com>
2021-10-26 16:15:17 +0000

    add file

---
+++ JS-study/arithmetic.js
@@ -0,0 +1,24 @@
+//sum of two numbers
+function calcSum(a, b) {
+    console.log("sum of two number is,",a+b);
+}
+calcSum(5, 6);
+
+//multiple of two numbers
+function calcMulitple(a,b){
+    console.log("mulitple of two numbers is,",a*b);
+}
+calcMulitple(4,3);
+
+
+//subraction of two numbers
+function calcSubraction(a,b){
+    console.log("subraction of two numbers is,",a-b);
+}
+calcSubraction(8,4);
+
+//division of two numbers
+function calcDivision(a,b){
+    console.log("division of two numbers is,",a/b);
+}
+calcDivision(33,3);
