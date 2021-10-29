0b566243e823e07f8046de4203eae76b16df64b8
Vaishnavi <vaishnavikumarasamy5@gmail.com>
2021-10-29 20:39:51 +0000

    added the code

--- JS-study/arithmetic.js
+++ JS-study/arithmetic.js
@@ -6,40 +6,50 @@ function CheckIsNumber(input) {
 }
 
 //sum of two numbers
-
 let sum = (a, b) => {
   CheckIsNumber(a);
   CheckIsNumber(b);
   return a + b;
 };
-console.log("sum of two number is,", sum(23, 9));
 
 //subraction of two numbers
-
 let subraction = (a, b) => {
   CheckIsNumber(a);
   CheckIsNumber(b);
   return a - b;
 };
-console.log("subraction of two number is,", subraction(5, 6));
 
 //multiplication of two numbers
-
 let multiple = (a, b) => {
   CheckIsNumber(a);
   CheckIsNumber(b);
   return a * b;
 };
-console.log("multiple of two number is,", multiple(6, 9));
 
 //division of two numbers
-
 let division = (a, b) => {
   CheckIsNumber(a);
   CheckIsNumber(b);
   return a / b;
 };
-console.log("division of two number is,", division(56, 9));
+//calculating
+function result(calculate) {
+  switch (true) {
+    case calculate == sum:
+      console.log("sum of two number is,", sum(9, 8));
+      break;
+    case calculate == subraction:
+      console.log("subraction of two number is,", subraction(5,9));
+      break;
+    case calculate == multiple:
+      console.log("multiple of two number is,", multiple(6, 9));
+      break;
+    case calculate == division:
+      console.log("division of two number is,", division(56, 9));
+  }
+}
+result(sum);
 
+//the out put will be
 
-
+//sum of two number is 17
